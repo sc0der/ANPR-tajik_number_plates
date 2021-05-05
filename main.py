ecdfe3ac3798f3ce97df0712629f32022340f1da
Sirius Sufiew <44986984+sc0der@users.noreply.github.com>
2021-05-05 08:54:45 +0500

    reinitialization

--- main.py
+++ main.py
@@ -1,77 +1,52 @@
-import numpy as np
-import cv2 as cv
-from const import videoPath
 
-vidcap = cv.VideoCapture(videoPath)
-# plates_cascade = cv.CascadeClassifier('haarcascade_russian_plate_number.xml')
+from const import videoPath
+import cv2 as cv2
+import numpy as np
+
 width = 1000
 height = 1000
-vidcap.set(cv.CAP_PROP_FRAME_WIDTH, width)
-vidcap.set(cv.CAP_PROP_FRAME_HEIGHT, height)
-ret, frame1 = vidcap.read()
 vehicle = 0
 safina = 0
 ashan = 0
 zeleniy = 0
 
-while vidcap.isOpened():
-    ret, frame2 = vidcap.read()
-    frame = frame2.copy()
-    if not ret:
-        break
-    fgMask = cv.absdiff(frame1, frame2)
-    fgMask = cv.cvtColor(fgMask, cv.COLOR_RGB2GRAY)
-    _, thresh = cv.threshold(fgMask, 50, 255, cv.THRESH_BINARY)
-    frame1 = frame2
-
+face_cascade = cv2.CascadeClassifier('.\haarcascades_number\haarcascade_russian_plate_number.xml')
+vidcap = cv2.VideoCapture(videoPath)
+vidcap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+vidcap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+scaling_factor = 1
+while True:
+    ret, frame = vidcap.read()
+    try:
+        frame = cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+    except:
+        vidcap = cv2.VideoCapture(videoPath)
+        continue
+    face_rects = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=3)
+    for (x,y,w,h) in face_rects:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+    
     # draw lines
-    cv.line(frame, (700, 100), (1020, 100), (0,0,255), 2) #  first plate line To Safina
-    cv.line(frame, (200, 460), (1200, 480), (0,0,255), 2) # second plate line from Main road
+    # first plate line To Safina
+    cv2.line(frame, (700, 100), (1020, 100), (0, 0, 255), 2)
+    # second plate line from Main road
+    cv2.line(frame, (200, 460), (1200, 480), (0, 0, 255), 2)
 
-    cv.line(frame, (500, 200), (600, 100), (0,255,0), 2) # down line of traps To Zeleniy
-    cv.line(frame, (1100, 250), (1050, 110), (0,255, 0), 2) # down line of traps To Ashan
-
-    # extract contours
-    conts, _ = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    for c in conts:
-        
-        #ignore small contours
-        if cv.contourArea(c) < 100:
-            continue
-        x, y, w, h = cv.boundingRect(c)
-        print("h: ", h)
-        print("w: ", w)
-        print("y: ", y)
-        print("x: ", x)
-        #draw  thr bound rect
-        if w > 30:
-            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            xMiddle = int((x+(x+w))/2)
-            yMiddle = int((y+(y+h))/2)
-            cv.circle(frame, (xMiddle, yMiddle), 5, (0, 0, 255), 5)
-            if yMiddle > 450 and yMiddle<480:
-                vehicle += 1
-            elif xMiddle < 500 and xMiddle > 490 and yMiddle < 200:
-                zeleniy += 1
-            elif xMiddle > 1050 and xMiddle < 1100 and yMiddle < 250:
-                ashan += 1
-            elif yMiddle > 90 and yMiddle < 100:
-                safina += 1
-
-
-    # cv.imshow("foreground Mask", fgMask)
-    cv.putText(frame, "--> Ashan: {}".
-    format(ashan), (1050, 100), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-    cv.putText(frame, "<-- Zeleniy: {}".
-    format(zeleniy), (200, 200), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-    cv.putText(frame, " Main road: {}".
-    format(vehicle), (100, 400), cv.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-    cv.putText(frame, "Safina: {}".
-    format(safina), (700, 80), cv.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-    cv.imshow("Original video", frame)
-    if cv.waitKey(1) & 0xFF == ord("q"):
+    # down line of traps To Zeleniy
+    cv2.line(frame, (500, 200), (600, 100), (0, 255, 0), 2)
+    # down line of traps To Ashan
+    cv2.line(frame, (1100, 250), (1050, 110), (0, 255, 0), 2)
+    cv2.putText(frame, "--> Ashan: {}".
+    format(ashan), (1050, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    cv2.putText(frame, "<-- Zeleniy: {}".
+    format(zeleniy), (200, 200), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    cv2.putText(frame, " Main road: {}".
+    format(vehicle), (100, 400), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+    cv2.putText(frame, "Safina: {}".
+    format(safina), (700, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+    cv2.imshow('Car Detector', frame)
+    c = cv2.waitKey(1)
+    if c == 27:
         break
-
-cv.destroyAllWindows()
 vidcap.release()
-
+cv2.destroyAllWindows()
